d92bc12b3c9e2a04abaa1747acece4d07074688f
davidGasanov <tansacal@protonmail.com>
2025-09-22 11:31:43 +0400

    finalize about

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,11 +1,15 @@
+import MailingSection from "../../components/mailing-section";
 import AboutImageSection from "./sections/image-section";
 import AboutSection1 from "./sections/sect-1";
+import AboutSection2 from "./sections/sect-2";
 
 const AboutPage = () => {
   return (
     <>
       <AboutSection1 />
       <AboutImageSection />
+      <AboutSection2 />
+      <MailingSection />
     </>
   );
 };
