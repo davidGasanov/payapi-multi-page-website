540ab068e71f69bf35dc637ca39b741fe939a99f
davidGasanov <tansacal@protonmail.com>
2025-09-24 12:25:40 +0400

    adjust circle styles

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -21,7 +21,9 @@ const socials = [
 
 const Footer = () => {
   return (
-    <footer className="bg-mirage-blue py-10 mt-10 md:py-[31px] md:mt-[100px] lg:mt-[96px] relative z-10">
+    <footer className="bg-mirage-blue py-10 mt-10 md:py-[31px] md:mt-[100px] lg:mt-[96px] relative z-10 overflow-hidden">
+      {/* Bg image */}
+      <div className="absolute w-[780px] h-[780px] bg-san-juan-blue/15 rounded-full -z-10 top-[177px] left-1/2 -translate-x-1/2 md:-right-[390px] md:translate-x-0 md:-top-[136px] md:left-auto" />
       <Container className="flex flex-col items-center md:flex-row md:items-center">
         <img src="/assets/shared/desktop/logo-white.svg" />
         <nav
